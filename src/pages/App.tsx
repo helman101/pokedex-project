0dbdd3b603bb633855f76d325815ae065bc38e56
helman101 <helmanandresortegon@gmail.com>
2024-01-02 16:46:55 +0100

    FEAT: add PokemonList component to App and fetch the first list of pokemon

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,40 @@
+import { useContext, useEffect } from 'react'
+import { PokemonList } from '../Components/PokemonList'
+import '../style.scss'
+import { getFirstPokemonList } from '../services/pokeApi'
+import { type usePokeStoreI, type PokemonFromList } from '../utils/types'
+import { PokeContext } from '../Context/PokeContext'
+
 export const App = () => {
+  const {
+    pokemonList,
+    loadingPokemonList,
+    setLoadingPokemonList,
+    setPokemonList,
+    setNextListUrl
+  } = useContext<usePokeStoreI>(PokeContext)
+
+  const getPokemonList = async () => {
+    setLoadingPokemonList()
+    await getFirstPokemonList()
+      .then((res: { results: PokemonFromList[], next: string }) => {
+        setPokemonList(false, res.results)
+        setNextListUrl(res.next)
+      })
+  }
+
+  useEffect(() => {
+    void getPokemonList()
+  }, [])
+
   return (
-    <div>App</div>
+    <main>
+      <section>
+        <PokemonList
+          loading={loadingPokemonList}
+          list={pokemonList}
+        />
+      </section>
+    </main>
   )
 }
