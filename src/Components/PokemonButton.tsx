dffb69a2a6818422e562617dcf1a818a7678ac0b
helman101 <helmanandresortegon@gmail.com>
2024-01-08 16:45:01 +0100

    FEAT: change PokemonButton styling & typing

--- src/Components/PokemonButton.tsx
+++ src/Components/PokemonButton.tsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import { type PokemonFromList } from '../utils/types'
+import { type ItemFromList } from '../utils/types'
 
 interface Props {
   id: number
-  pokemon: PokemonFromList
+  pokemon: ItemFromList
   selected?: boolean
   onClick: () => void
 }
 
 export const PokemonButton = ({ id, pokemon, selected, onClick }: Props) => {
   return (
-    <div className={`${selected ? 'selected' : ''} btn-wrapper p-1 rounded`}>
+    <div className={`${selected ? 'selected' : ''} btn-wrapper p-1 rounded w-100`}>
       <button
-        className={`${selected ? 'selected' : ''} pokemon-btn btn digital-font d-flex py-4 px-4 rounded`}
+        className={`${selected ? 'selected' : ''} pokemon-btn btn game-font d-flex align-items-center py-4 px-3 rounded w-100`}
         onClick={onClick}
         >
-        <h4 className='mb-0'>{`N° ${id}`}</h4>
+        <small className='mb-0'>{`N°${id}`}</small>
         <div className='flex-fill'>
-          <h4 className='mb-0'>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h4>
+          <small className='mb-0'>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</small>
         </div>
       </button>
     </div>
