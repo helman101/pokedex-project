8295a1984fed25e673b1002bc9eb478cf1a92dd0
helman101 <helmanandresortegon@gmail.com>
2024-01-11 16:29:20 +0100

    FEAT: add Filters component & transfer pokemon list calls to it

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,31 +1,14 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { PokemonList } from '../Components/PokemonList'
 import '../style.scss'
-import { getFirstPokemonList } from '../services/pokeApi'
-import { type usePokeStoreI, type ItemFromList } from '../utils/types'
+import { type usePokeStoreI } from '../utils/types'
 import { PokeContext } from '../Context/PokeContext'
 
 export const App = () => {
   const {
     pokemonList,
-    loadingPokemonList,
-    setLoadingPokemonList,
-    setPokemonList,
-    setNextListUrl
+    loadingPokemonList
   } = useContext<usePokeStoreI>(PokeContext)
-
-  const getPokemonList = async () => {
-    setLoadingPokemonList()
-    await getFirstPokemonList()
-      .then((res: { results: ItemFromList[], next: string }) => {
-        setPokemonList(false, res.results)
-        setNextListUrl(res.next)
-      })
-  }
-
-  useEffect(() => {
-    void getPokemonList()
-  }, [])
 
   return (
     <main>
