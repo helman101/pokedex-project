953931df3169376d43fad66c7024dc7b9672cc99
helman101 <helmanandresortegon@gmail.com>
2024-01-11 16:30:51 +0100

    FEAT: add Filters to PokemonList component

--- src/Components/PokemonList.tsx
+++ src/Components/PokemonList.tsx
@@ -1,17 +1,28 @@
-import { useContext, useRef } from 'react'
-import { type PokemonFromList } from '../utils/types'
+import { useContext, useEffect, useRef } from 'react'
+import { type ItemFromList } from '../utils/types'
 import { PokemonButton } from './PokemonButton'
 import { PokeContext } from '../Context/PokeContext'
 import { getPokemonListFromUrl } from '../services/pokeApi'
 import { Loader } from './Loader'
+import { Filters } from './Filters/Filters'
 
 interface Props {
-  list: PokemonFromList[]
+  list: ItemFromList[]
   loading: boolean
 }
 
 export const PokemonList = ({ list, loading }: Props) => {
-  const { loadingInfinityScroll, currentPokemon, nextListUrl, setCurrentPokemon, setPokemonList, setNextListUrl, setLoadingInfinityScroll } = useContext(PokeContext)
+  const {
+    loadingPokemonList,
+    loadingInfinityScroll,
+    currentPokemon,
+    nextListUrl,
+    setCurrentPokemon,
+    setPokemonList,
+    setNextListUrl,
+    setLoadingInfinityScroll
+  } = useContext(PokeContext)
+
   const listRef = useRef<HTMLInputElement>(null)
 
   const listScroll = () => {
@@ -21,7 +32,7 @@ export const PokemonList = ({ list, loading }: Props) => {
         if (nextListUrl) {
           setLoadingInfinityScroll()
           void getPokemonListFromUrl(nextListUrl)
-            .then((res: { results: PokemonFromList[], next: string }) => {
+            .then((res: { results: ItemFromList[], next: string }) => {
               setPokemonList(true, res.results)
               setNextListUrl(res.next)
             })
@@ -30,24 +41,36 @@ export const PokemonList = ({ list, loading }: Props) => {
     }
   }
 
+  useEffect(() => {
+    if (loadingPokemonList) {
+      listRef.current?.scrollTo({ top: 0 })
+      setCurrentPokemon({ id: 1 })
+    }
+  }, [loadingPokemonList])
+
   return (
     <div className='w-100 d-flex justify-content-center'>
-      <div
-        className='pokemon-list rounded d-flex flex-column align-items-center position-relative'
-        onScroll={listScroll}
-        ref={listRef}
-      >
-        {loading && <Loader />}
-        {!loading &&
-          list.map((item: PokemonFromList, i: number) =>
-            <PokemonButton
-              key={i}
-              id={i + 1}
-              onClick={() => { setCurrentPokemon({ id: i + 1 }) } }
-              pokemon={item}
-              selected={ currentPokemon?.id === i + 1 }/>
-          )}
-        {loadingInfinityScroll && <Loader />}
+      <div className='row pokemon-list rounded'>
+        <div
+          className='col-6 pokemon-col pt-2 d-flex flex-column align-items-center position-relative'
+          onScroll={listScroll}
+          ref={listRef}
+        >
+            {loading && <Loader />}
+            {!loading &&
+              list.map((item: ItemFromList, i: number) =>
+                <PokemonButton
+                  key={i}
+                  id={i + 1}
+                  onClick={() => { setCurrentPokemon({ id: i + 1 }) } }
+                  pokemon={item}
+                  selected={ currentPokemon?.id === i + 1 }/>
+              )}
+            {loadingInfinityScroll && <Loader />}
+        </div>
+        <div className='col'>
+          <Filters />
+        </div>
       </div>
     </div>
   )
