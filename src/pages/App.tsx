b4225b571f6c3595993afbe5c8b0e3833363a8e9
helman101 <helmanandresortegon@gmail.com>
2024-01-25 16:46:55 +0100

    FEAT: add PokemonInfo component to App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,6 +3,7 @@ import { PokemonList } from '../Components/PokemonList/PokemonList'
 import '../style.scss'
 import { type usePokeStoreI } from '../hooks/PokeStore/types'
 import { PokeContext } from '../Context/PokeContext'
+import { PokemonInfo } from '../Components/PokemonInfo/PokemonInfo'
 
 export const App = () => {
   const {
@@ -11,7 +12,10 @@ export const App = () => {
   } = useContext<usePokeStoreI>(PokeContext)
 
   return (
-    <main>
+    <main className='game-font'>
+      <section>
+        <PokemonInfo />
+      </section>
       <section>
         <PokemonList
           loading={loadingPokemonList}
