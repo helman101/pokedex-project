2cb1610e92d70fd5704d593126aaab7957b2977c
helman101 <helmanandresortegon@gmail.com>
2024-02-13 10:25:19 +0100

    FEAT: add details to top screen

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -14,10 +14,20 @@ export const App = () => {
   return (
     <main className='game-font'>
       <section>
-        <div className='top-screen d-flex justify-content-center align-items-center border-bottom-0'>
-          <div className="ds-frame rounded-3">
-            <PokemonInfo />
+        <div className='top-screen-margin m-auto position-relative'>
+          <div className="pad left position-absolute" />
+          <div className='top-screen d-flex justify-content-center align-items-center border-bottom-0'>
+            <div className="points" />
+            <div className="square top right position-absolute rounded-2" />
+            <div className="square top left position-absolute rounded-2" />
+            <div className="ds-frame rounded-3">
+              <PokemonInfo />
+            </div>
+            <div className="square bottom right position-absolute rounded-2" />
+            <div className="square bottom left position-absolute rounded-2" />
+            <div className="points" />
           </div>
+          <div className="pad right position-absolute rounded-1" />
         </div>
       </section>
       <section>
