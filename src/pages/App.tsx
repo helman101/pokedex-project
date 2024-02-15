4480655261c2192ed8f6fbe5438ced05c38a7317
helman101 <helmanandresortegon@gmail.com>
2024-02-15 14:58:44 +0100

    FEAT: add elements for middle section and add styles to it

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -31,6 +31,17 @@ export const App = () => {
         </div>
       </section>
       <section>
+        <div className='middle m-auto rounded-pill mt-1 position-relative d-flex justify-content-around align-items-center'>
+          <div className='separator left position-absolute' />
+          <div className='mic rounded-pill' />
+          <div className='separator right position-absolute' />
+          <div className='battery-container h-100 d-flex position-absolute d-flex align-items-center'>
+            <div className="battery off rounded-pill" />
+            <div className="battery on rounded-pill" />
+          </div>
+        </div>
+      </section>
+      <section>
         <PokemonList
           loading={loadingPokemonList}
           list={pokemonList}
